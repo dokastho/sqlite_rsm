4970795dbfc041605061213f9689e3b2d84c424d
Thomas Dokas <dokastho@umich.edu>
2022-09-13 19:04:50 +0000

    include initial repo setup

---
+++ sqlite-rsm/__main__.py
@@ -0,0 +1,6 @@
+'''File for launching the rsm fragment.'''
+
+# fetch schema from url using token
+# download it
+# start the sqlite db
+# connect to a master if one exists
---
+++ sqlite-rsm/config.py
@@ -0,0 +1,5 @@
+# config file for the rsm fragment
+
+# url endpoint for fetching the db schema
+SCHEMA_TOKEN = ''
+SCHEMA_URL = ''
