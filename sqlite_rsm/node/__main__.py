3348710d5360cf4a7a4e6d200de8cf8476edba61
Thomas Dokas <dokastho@umich.edu>
2022-09-14 03:36:46 +0000

    consolidate node source code into node class

--- sqlite_rsm/node/__main__.py
+++ sqlite_rsm/node/__main__.py
@@ -1,8 +1,8 @@
 '''File for launching the rsm fragment.'''
 
 import pathlib
-from traceback import print_tb
-# import sqlite_rsm
+import sqlite3
+import flask
 import sqlite_rsm.node as node
 
 
@@ -23,8 +23,59 @@ class Node:
         db_file = pathlib.Path(db)
         if not db_file.is_file():
             db_file.touch()
+        
+        # start the db (this step might be wrong)
+        self.get_db()
 
         node.app.run(port=0)
+        
+    def dict_factory(self, cursor, row):
+        """Convert database row objects to a dictionary keyed on column name.
+
+        This is useful for building dictionaries which are then used to render a
+        template.  Note that this would be inefficient for large queries.
+        """
+        return {col[0]: row[idx] for idx, col in enumerate(cursor.description)}
+
+
+
+    def get_db(self):
+        """Open a new database connection.
+
+        Flask docs:
+        https://flask.palletsprojects.com/en/1.0.x/appcontext/#storing-data
+        """
+        with node.app.app_context(self):
+            if 'sqlite_db' not in flask.g:
+                db_filename = node.app.config['DATABASE_FILENAME']
+                flask.g.sqlite_db = sqlite3.connect(str(db_filename))
+                flask.g.sqlite_db.row_factory = self.dict_factory
+                # Foreign keys have to be enabled per-connection.  This is an sqlite3
+                # backwards compatibility thing.
+                flask.g.sqlite_db.execute("PRAGMA foreign_keys = ON")
+            return flask.g.sqlite_db
+
+
+    @node.app.teardown_appcontext
+    def close_db(self, error):
+        """Close the database at the end of a request.
+
+        Flask docs:
+        https://flask.palletsprojects.com/en/1.0.x/appcontext/#storing-data
+        """
+        assert error or not error  # Needed to avoid superfluous style error
+        sqlite_db = flask.g.pop('sqlite_db', None)
+        if sqlite_db is not None:
+            sqlite_db.commit()
+            sqlite_db.close()
+            
+    # @sqlite_rsm.app.route('/', methods=['POST'])
+    @node.app.route('/', methods=['POST'])
+    def serve_db(self):
+        """main route for interacting with the db."""
+        pass
+
+
 
 
 def main():
--- sqlite_rsm/node/endpoint.py
+++
@@ -1,7 +0,0 @@
-import sqlite_rsm
-import flask
-
-@sqlite_rsm.app.route('/', methods=['POST'])
-def serve_db():
-    """main route for interacting with the db."""
-    pass
